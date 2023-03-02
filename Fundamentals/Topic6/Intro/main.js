714ed206065f8bc1def42ca889bbf63cb96eba96
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2023-03-02 21:01:17 +0400

    update

---
+++ Fundamentals/Topic6/Intro/main.js
@@ -0,0 +1,76 @@
+// Data Type
+// Primitive - Integer, String, Boolean, Nullish, Undefined
+// InPrimitive - Array, Object, Function
+
+//Integer
+// var price = 5;
+// var raiting = 6.7;
+
+// // String
+// var fullname = "Ibrahim Seyidov";
+// var name = "6.7";
+
+// //Boolean
+// var baseKind = true;
+// var married = false;
+
+// //Nullish
+// var passportSekili = null;
+
+// //Undefined
+// var personnalInfo = undefined;
+
+// console.log(fullname);
+// console.log(raiting);
+// console.log(name);
+
+// var a = 35;
+
+// var b = 7;
+
+// var c = 2 + 1;
+
+// var string = "12";
+// var string = "Hello";
+
+// var d = a;
+
+// var d = a - c;
+
+// var d = string + b;
+// var d = string - b;
+
+// var netice = 12 % 2;
+
+// var x = 5;
+
+// // x = x + 12;
+// x += 12;
+
+// var x = 5;
+
+// var z = 5;
+
+// var netice = x == z;
+// var netice = x === z;
+// var netice = x !== z;
+// var netice = x <= z;
+// var netice = x;
+
+// 0
+// ""
+// false
+// null
+// undefined
+
+// var a = 0;
+
+// var netice = !a;
+
+// var a = false;
+
+// var b = 6;
+
+// var netice = a && b;
+// var netice = a || b;
+console.log(netice);
