90b15e389a32696dd21264bafda9985079acd3b7
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2023-03-09 21:01:36 +0400

    updatE

---
+++ Fundamentals/Topic6/Intro/string.js
@@ -0,0 +1,77 @@
+// var text = "r/u/s/L-a-N";
+// var text = "ruslana";
+
+// var arr = [2,3,4,4]
+
+// console.log(text[1]);
+// console.log(text.length);
+
+// var result = text.endsWith("n"); //true
+// var result = text.startsWith("R"); //true
+// var result = text.toUpperCase() //RUSLAN
+// var result = text.toLowerCase(); //ruslan
+// var result = text.toLowerCase(); //ruslan
+// var result = text.toLowerCase(); //ruslan
+// var result = text.repeat(3); //ruslan
+// var result = text.split("/"); //ruslan
+// var result = text.lastIndexOf("a"); //ruslan
+// var result = text.replace("a", "e"); //ruslan
+// var result = text.search("aaarus"); //ruslan
+
+// console.log(result);
+
+// var arr = text.split("");
+// var newText = "";
+
+// console.log("arr", arr);
+
+// for (var letter of arr) {
+//   if (letter === "a") {
+//     newText += "e";
+//   } else {
+//     newText = newText + letter;
+//   }
+// }
+
+// for()
+
+// console.log(newText);
+
+// if (text.startsWith("S")) {
+//   console.log("Size qurban olum");s
+// }
+
+// var text = "Hellooooooooooo";
+
+// var searcLetter = "o";
+
+// var count = 0;
+
+// var letterArray = text.split(""); // ["H","e","l","l","o"]
+
+// for (var index in letterArray) {
+//   if (letterArray[index] === searcLetter) {
+//     // count = count + 1
+//     count += 1;
+//   }
+// }
+
+// var resultText = `Bu "${text}" sozunun icinde ${count} defe ${searcLetter} herfi isledilmisdir`;
+
+// console.log(resultText);
+
+// var a = true;
+// var b = true;
+
+// if ("4" === 4) {
+//   alert("puppies triple ===");
+// } else {
+//   alert("goats");
+// }
+
+// 1. Sual verilir "Steak xosluyursan?"
+// 2.Eger Beli dese Yas sorusulur. Ve yas 21den asagidirsa "Size yemek olmaz" deyilir
+// 3.Eks halda ise "Kartof fri isteyirsizmi?" suali verirlir.
+// 4.Beli dese "Yanininda hansi sous olsun suali verilir" Xeyr dese nus olsun deyilir
+// .Programda Kartof friyede beli deyen sous adinida yazan istifadeciye sonra alert mesaji
+// "Dadli filan souslu friniz steak ile artiq hazirlanir" alert mesaji verilsin
