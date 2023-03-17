59983430c0332aa71b70fc795e1e0dc6f1762500
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2023-03-17 12:18:35 +0400

    update

--- Fundamentals/Topic6/Intro/function.js
+++ Fundamentals/Topic6/Intro/function.js
@@ -1,10 +1,14 @@
+// Input and Output
+
 // var c = 15;
 
-// function b(a, b, yeniDeyer) {
+// function b(a, b, yeniDeyer, paramArray) {
+//   console.log("param", paramArray);
+
 //   console.log("sum:", (a + b) * yeniDeyer);
 // }
 
-// b(3, 4, c);
+// b(3, 4, c, [1, 2]);
 // b(10, 5, c);
 // b(12, 7, c);
 // b(125, 2, c);
@@ -66,32 +70,32 @@
 // }
 
 // function guessHolidays() {
-//   var month = prompt("Which do you want month?");
-//   var day = prompt("Which do you want day?");
+// var month = prompt("Which do you want month?");
+// var day = prompt("Which do you want day?");
 
-//   switch (month) {
-//     case "january":
-//       console.log("Bu ayda bayram yoxdur.");
-//       console.log("Bu ayda bayram yoxdur.");
-//       break;
+// switch (month) {
+//   case "january":
+//     console.log("Bu ayda bayram yoxdur.");
+//     console.log("Bu ayda bayram yoxdur.");
+//     break;
 
-//     case "february":
-//       console.log("14 februrary");
-//       break;
+//   case "february":
+//     console.log("14 februrary");
+//     break;
 
-//     case "march":
-//       if (day == 8) {
-//         console.log("8 march");
-//       } else if (day == 20) {
-//         console.log("20 Newrouz");
-//       }
+//   case "march":
+//     if (day == 8) {
+//       console.log("8 march");
+//     } else if (day == 20) {
+//       console.log("20 Newrouz");
+//     }
 
-//       break;
+//     break;
 
-//     default:
-//       console.log("Biz bayram qeyd etmirik");
-//       break;
-//   }
+//   default:
+//     console.log("Biz bayram qeyd etmirik");
+//     break;
+// }
 // }
 
 // guessHolidays();
@@ -142,18 +146,149 @@
 
 // console.log(result);
 
-function noSpace(x) {
-  var arrString = x.split(" ");
-  return arrString.join("");
+// function noSpace(string, num1, num2) {
 
-  //   var text = "";
+//   console.log(arguments);
 
-  //   for (var i = 0; i < arrString.length; i++) {
-  //     if (arrString[i] !== " ") {
-  //       text += arrString[i];
-  //     }
-  //   }
-  //   return text;
+//   // if (x === "melikmemmed") {
+//   //   return 6;
+//   // }
+
+//   // if (typeof x !== string) {
+//   //   return -1;
+//   // }
+
+//   // var a = 2;
+
+//   return 4 + a;
+
+//   // var arrString = x.split(" ");
+//   // return arrString.join("");
+//   // return 5;
+//   // console.log(arrString.join(""));
+//   //   var text = "";
+//   //   for (var i = 0; i < arrString.length; i++) {
+//   //     if (arrString[i] !== " ") {
+//   //       text += arrString[i];
+//   //     }
+//   //   }
+//   //   return text;
+// }
+
+// var result = noSpace("8 j 8   mBliB8g  imjB8B8  jl  B", 4, 5);
+
+// var arr1 = [1, 2];
+
+// var arr2 = ["Banana", "Watermaloon", "Apple", "Pie"];
+
+// // var arr3 = arr1.concat(arr2);
+// var arr3 = [...arr2, ...arr1]; // spread
+
+// // arr3[0] = "Hello";
+
+// console.log("arr3", arr3);
+
+// function read(a, b, ...params) {
+//   //rest operator
+
+//   // var value = "";
+
+//   console.log("a", a);
+//   console.log("b", b);
+//   console.log("params", params);
+// }
+
+// read(1, 2, 3, 4, 5, "Hello", true, null);
+
+// read(...arr2);
+
+// (function sum(a, b) {
+//   console.log("sum:", a + b);
+// })(4, 7);
+
+// function sum(a, b) {
+//   console.log("sum:", a + b);
+// };
+
+// sum()
+
+// var arr1 = [1, 2, 3, 4, 5, 6, 7];
+// var k = 3;
+
+// function changeElement(array, n) {
+//   for (let index = 0; index < n; index++) {
+//     var result = array.shift();
+//     array.push(result);
+//   }
+
+//   console.log("array", array);
+// }
+
+// changeElement(arr1, k);
+
+// sum(3)(5)(7)
+
+// var array = [
+//   function b() {
+//     return 5 + 6;
+//   },
+//   2,
+//   5,
+//   "Hello",
+//   [1, 2],
+// ];
+
+// array[3].toUpperCase();
+
+// function sum(num1) {
+//   return function (num2) {
+//     return function (num3) {
+//       return num1 + num2 + num3;
+//     };
+//   };
+// }
+
+// // var result1 = sum(5);
+
+// // var result = sum(5)(6)(7);
+
+// // console.log(result1(6));
+// console.log(result);
+
+// (function sum(a, b) {
+//   return a + b;
+// })(5, 6);
+
+// Various Arrays
+var brands = ["Acer", "Apple", "Sony", "Samsung"];
+var heroes = [
+  "Black Panther",
+  "Cyborg",
+  "Black Canary",
+  "Donna Troy",
+  "Huntress",
+  "Blue Beetle",
+  "Captain Atom",
+];
+var booksOnMyShelf = [
+  "Calculus Early Transcendentals",
+  "Ravens",
+  "The Self Illusion",
+  "Harry Potter",
+];
+var thingsInFrontOfMe = ["Laptop", "Beanbag", "Cats", "Slippers"];
+var howIFeel = ["Sleep Deprived", "Wired on Coffee", "Excited"];
+
+function brandsCap(array) {
+  // For Loop for Heroes
+  for (var j = 0; j < array.length; j++) {
+    console.log(array[j]);
+  }
+  console.log("---------");
 }
 
-var result = noSpace("8 j 8   mBliB8g  imjB8B8  jl  B");
+brandsCap(brands);
+brandsCap(heroes);
+brandsCap(booksOnMyShelf);
+brandsCap(thingsInFrontOfMe);
+brandsCap(howIFeel);
