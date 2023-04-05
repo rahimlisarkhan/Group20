f9d56d831514ef97f2512a147ea91dfb4a6f9e8f
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2023-04-05 19:40:44 +0400

    update

--- Fundamentals/Topic6/TodoApp/main.js
+++ Fundamentals/Topic6/TodoApp/main.js
@@ -3,6 +3,8 @@ var todoInput = document.querySelector("#todoInput");
 var todoAddButton = document.querySelector("#todoAddButton");
 var todoClearButton = document.querySelector("#todoClearButton");
 
+var todoAlert = document.getElementById("todoAlert");
+
 var todoList = document.querySelector("#todoList");
 
 var todoListData = [];
@@ -19,6 +21,50 @@ function addTodoItem() {
   todoListData.push(value);
 
   todoInput.value = "";
+
+  console.log(todoAlert);
+
+  todoAlert.innerHTML = "Succesfully added";
+  todoAlert.classList.remove("d-none");
+  todoAlert.classList.remove("alert-danger");
+
+  // todoAlert.classList.add("d-block");
+  todoAlert.classList.add("alert-success");
+  todoAlert.classList.add("d-block");
+
+  showTodoItems();
+}
+
+function removeItem(index) {
+  console.log("index", index);
+
+  var isRemove = confirm("Did you want remove item?");
+
+  if (!isRemove) {
+    return;
+  }
+
+  newArrayData = todoListData.filter(function (item, elmenetIndex, list) {
+    if (elmenetIndex !== index) return true;
+
+    return false;
+  });
+
+  todoListData = newArrayData;
+
+  todoAlert.innerHTML = "Unfortunally removed item";
+  todoAlert.classList.remove("d-none");
+  todoAlert.classList.remove("alert-success");
+
+  todoAlert.classList.add("d-block");
+  todoAlert.classList.add("alert-danger");
+
+  showTodoItems();
+}
+
+function clearList() {
+  todoListData = [];
+
   showTodoItems();
 }
 
@@ -35,21 +81,25 @@ function showTodoItems() {
 
   //solition2
 
-  todoList.innerHTML = todoListData
+  var content = todoListData
     .map(function (item, index) {
       return `<li class="list-group-item d-flex justify-content-between">
-      ${index + 1}. ${item}
-      <button
-        type="button"
-        class="btn btn-danger btn-sm"
-        id="todoRemoveButton"
-        onclick="removeItem(${index})"
-      >
-        remove
-      </button>
-    </li>`;
+    ${index + 1}. ${item}
+    <button
+      type="button"
+      class="btn btn-danger btn-sm"
+      id="todoRemoveButton"
+      onclick="removeItem(${index})"
+    >
+      remove
+    </button>
+  </li>`;
     })
     .join("");
+
+  console.log(content);
+
+  todoList.innerHTML = content;
 
   //solition3
   //   todoList.innerHTML = "";
@@ -63,26 +113,6 @@ function showTodoItems() {
   //   }
 
   console.log("todoListData: ", todoListData);
-}
-
-function removeItem(index) {
-  newArrayData = todoListData.filter(function (_, elmenetIndex) {
-    if (elmenetIndex !== index) return true;
-
-    return false;
-  });
-
-  console.log("newArrayData", newArrayData);
-
-  todoListData = newArrayData;
-
-  showTodoItems();
-}
-
-function clearList() {
-  todoListData = [];
-
-  showTodoItems();
 }
 
 //Events
