eeedb931ccb2eecf5b3f45a98a7056067f66773e
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-06-13 22:10:51 +0400

    update

---
+++ Fundamentals/Topic15/intro/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWE7Q6rMIE4saZ12pL1V2Kai76-XGT-7Y",
+  authDomain: "personalapp-f2c67.firebaseapp.com",
+  databaseURL:
+    "https://personalapp-f2c67-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "personalapp-f2c67",
+  storageBucket: "personalapp-f2c67.appspot.com",
+  messagingSenderId: "532763068687",
+  appId: "1:532763068687:web:02b523353add46aed4d67a",
+};
