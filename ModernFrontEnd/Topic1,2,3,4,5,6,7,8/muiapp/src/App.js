4cbd9c027724e44e0a0c252601027fd37bb18cc4
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-08-17 21:25:42 +0400

    updat

---
+++ ModernFrontEnd/Topic1,2,3,4,5,6,7,8/muiapp/src/App.js
@@ -0,0 +1,75 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+import { ProductCard } from "./ProductCard";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Avatar
+          alt="Remy Sharp"
+          // sizes="sm"
+          src="https://media.licdn.com/dms/image/D5603AQFd6eQTMGG8vg/profile-displayphoto-shrink_200_200/0/1680514093124?e=1695859200&v=beta&t=vD_ZrvkgQVobv2WAlQGvlyt-w4vhlEmiP2XisiUGdXk"
+        />
+        <Button
+          // startIcon={<Add />}
+          size="large"
+          variant="contained"
+          color="warning"
+        >
+          Send
+        </Button>
+        <Typography variant="body2" component="p">
+          h1. Heading
+        </Typography>
+        <ProductCard
+          img="https://media.licdn.com/dms/image/D5603AQFd6eQTMGG8vg/profile-displayphoto-shrink_200_200/0/1680514093124?e=1695859200&v=beta&t=vD_ZrvkgQVobv2WAlQGvlyt-w4vhlEmiP2XisiUGdXk"
+          title={"Product"}
+          desc="test"
+        />
+
+        <h1 className="text-orange-600 text-10xl text-mono "> Test1</h1>
+
+        <input className="focus:bg-blue-300 rounded border-none outline-none px-2" />
+
+        <div className="p-5 bg-white flex flex-col gap-4 rounded-lg  border border-yellow-300 shadow-md hover:bg-red-400 hover:translate-x-2 transition-all cursor-pointer">
+          <img src="https://media.licdn.com/dms/image/D5603AQFd6eQTMGG8vg/profile-displayphoto-shrink_200_200/0/1680514093124?e=1695859200&v=beta&t=vD_ZrvkgQVobv2WAlQGvlyt-w4vhlEmiP2XisiUGdXk" />
+          <p className="text-lg text-gray-300">Lorem ipsum</p>
+          <p className="text-sm text-gray-500">Lorem ipsum</p>
+        </div>
+
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div className="w-10 h-10 rounded-full overflow-hidden shadow">
+          <img
+            className="cover"
+            src="https://media.licdn.com/dms/image/D5603AQFd6eQTMGG8vg/profile-displayphoto-shrink_200_200/0/1680514093124?e=1695859200&v=beta&t=vD_ZrvkgQVobv2WAlQGvlyt-w4vhlEmiP2XisiUGdXk"
+          />
+        </div>
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <main>
+        <div className="w-1/4"></div>
+        <div className="w-3/4"></div>
+        <div></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
